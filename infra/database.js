306f6a0820ed48b43edb631c5b834da570049dd2
Ícaro Cazé Nunes <65853536+icarocnunes@users.noreply.github.com>
2025-06-25 21:08:21 +0000

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -9,23 +9,18 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-
-    // host: "localhost",
-    // port: 5432,
-    // user: "postgres",
-    // database: "postgres",
-    // password: "local_password",
-
-    //     POSTGRES_HOST=localhost
-    // POSTGRES_PORT=5432
-    // POSTGRES_PORT=postgres
-    // POSTGRES_DATABASE=postgres
-    // POSTGRESS_PASSWORD=local_password
   });
+
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error("Database connection error:", error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
